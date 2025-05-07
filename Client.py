7e659e818812dae277462db622c484f379341952
adminx95 <168667999+adminx95@users.noreply.github.com>
2025-05-07 22:08:48 +0530

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,33 +1,9 @@
-import socket
+import xmlrpc.client
 
+proxy = xmlrpc.client.ServerProxy('http://localhost:8000')
 
-def run_client():
-    # create a socket object
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+n = int(input("Enter any number: "))
 
-    server_ip = "127.0.0.1"  # replace with the server's IP address
-    server_port = 8001  # replace with the server's port number
-    # establish connection with server
-    client.connect((server_ip, server_port))
+result = proxy.compute_factorial(n)
 
-    while True:
-        # input message and send it to the server
-        msg = input("Enter message: ")
-        client.send(msg.encode("utf-8")[:1024])
-
-        # receive message from the server
-        response = client.recv(1024)
-        response = response.decode("utf-8")
-
-        # if server sent us "closed" in the payload, we break out of the loop and close our socket
-        if response.lower() == "closed":
-            break
-
-        print(f"Received: {response}")
-
-    # close client socket (connection to the server)
-    client.close()
-    print("Connection to server closed")
-
-run_client()
-
+print("Factorial:",result)
